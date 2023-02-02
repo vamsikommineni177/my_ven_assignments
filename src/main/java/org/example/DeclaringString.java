e938e931e0ee607db22b85544ab53b2f9441852f
vamsikommineni177 <vamsikommineni319@gmail.com>
2023-02-02 17:19:46 -0600

    my commits for ven assignments

---
+++ src/main/java/org/example/DeclaringString.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class DeclaringString {
+        public static void main(String[] args)
+        {
+            // Declare String without using new operator
+            String s = "GeeksforGeeks";
+            System.out.println("String "+ s+" is declared without using new operator so that the object will be created in string pool constant ");
+            String s1 = new String("GeeksforGeeks");
+            System.out.println("String " + s1+" is declared using new operator so that the memory allocation was occured in heap but not in string pool constant ");
+        }
+    }
+
